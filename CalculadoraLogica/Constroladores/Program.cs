fd1d0deeca045707307c8edccfd82f7c8e14327d
DESKTOP-736OHA5\Jesús <csi22-jalvugo@colegioaltair.net>
2023-10-10 17:55:46 +0200

    Agregar archivos de proyecto.

---
+++ CalculadoraLogica/Constroladores/Program.cs
@@ -0,0 +1,50 @@
+﻿using CalculadoraLogica.Servicios;
+
+
+namespace CalculadoraLogica
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+
+            MenuInterfaz mi = new MenuImplementacion();
+
+            int opcionSeleccionada;
+
+            bool cerrarMenu = false;
+
+            mi.MensajeBienvenida();
+            while (!cerrarMenu)
+            {
+                opcionSeleccionada = mi.MostrarMenuYSeleccion();
+
+                switch (opcionSeleccionada)
+                {
+                    case 0:
+
+                        Console.WriteLine("[INFO] se ejecutara la opcion 0");
+
+                        cerrarMenu = true;
+                        break;
+
+                    case 1:
+
+                        Console.WriteLine("[INFO] Se ejecutara la opcion 1");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("[INFO] se ejecutara la opcion 2");
+                        break;
+
+                    default:
+                        Console.WriteLine("[INFO] opcion introducida no es valida, por favor introduzca una opcion valida");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ CalculadoraLogica/Servicios/MenuImplementacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraLogica.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void MensajeBienvenida()
+        {
+            string mensaje = "Bienvenido a la calculadora lógica";
+
+            Console.WriteLine(mensaje);
+        }
+
+        public int MostrarMenuYSeleccion()
+        {
+            int opcion=9;
+            Console.WriteLine("\t\t\t╔══════════════════════════════╗");
+            Console.WriteLine("\t\t\t║             MENU             ║");
+            Console.WriteLine("\t\t\t╠══════════════════════════════╣");
+            Console.WriteLine("\t\t\t║                              ║");
+            Console.WriteLine("\t\t\t║      1.- Igualdad            ║");
+            Console.WriteLine("\t\t\t║      2.- Desigualdad         ║");
+            Console.WriteLine("\t\t\t║                              ║");
+            Console.WriteLine("\t\t\t║                              ║");
+            Console.WriteLine("\t\t\t║                              ║");
+            Console.WriteLine("\t\t\t║          0) Salir            ║");
+            Console.WriteLine("\t\t\t║                              ║");
+            Console.WriteLine("\t\t\t╚══════════════════════════════╝");
+            Console.Write("\t\tIntroduce una opción: ");
+
+            opcion = Console.ReadKey(true).KeyChar-('0');
+
+            return opcion;
+        }
+    }
+}
